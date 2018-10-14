84f3f9e2429a6cf2f276d0f4fc760b1f4e2d909f
Adam <adamandrys7@gmail.com>
2018-10-14 11:21:14 +0200

    BRAZactiv: change design (ListView, design of editText itp)

--- main/java/com/example/adam/rozpiskimeczowe/brazylian/BRAZactiv.java
+++ main/java/com/example/adam/rozpiskimeczowe/brazylian/BRAZactiv.java
@@ -1,18 +1,27 @@
 package com.example.adam.rozpiskimeczowe.brazylian;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.adam.rozpiskimeczowe.R;
 import com.example.adam.rozpiskimeczowe.brazylian.brazylian8.BRAZactiv8;
 
-public class BRAZactiv extends AppCompatActivity {
 
+
+public class BRAZactiv extends AppCompatActivity {
+    int quantityOfTeam = 8;
+    String[] names;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,116 +29,145 @@ public class BRAZactiv extends AppCompatActivity {
 
         getSupportActionBar().setTitle("System brazylijski");
 
-        final EditText name1 = findViewById(R.id.Name1);
-        final EditText name2 = findViewById(R.id.Name2);
-        final EditText name3 = findViewById(R.id.Name3);
-        final EditText name4 = findViewById(R.id.Name4);
-        final EditText name5 = findViewById(R.id.Name5);
-        final EditText name6 = findViewById(R.id.Name6);
-        final EditText name7 = findViewById(R.id.Name7);
-        final EditText name8 = findViewById(R.id.Name8);
+        ListView list = findViewById(R.id.listView);
 
-        name1.setVisibility(View.INVISIBLE);
-        name2.setVisibility(View.INVISIBLE);
-        name3.setVisibility(View.INVISIBLE);
-        name4.setVisibility(View.INVISIBLE);
-        name5.setVisibility(View.INVISIBLE);
-        name6.setVisibility(View.INVISIBLE);
-        name7.setVisibility(View.INVISIBLE);
-        name8.setVisibility(View.INVISIBLE);
+        list.setItemsCanFocus(true);
+
+        final CustomAdapter customAdapter = new CustomAdapter(this);
+        list.setAdapter(customAdapter);
 
 
-        final CheckBox box3 = (CheckBox) findViewById(R.id.checkBox3);
-        final CheckBox box4 = (CheckBox) findViewById(R.id.checkBox4);
-        final CheckBox box5 = (CheckBox) findViewById(R.id.checkBox5);
-        final CheckBox box6 = (CheckBox) findViewById(R.id.checkBox6);
 
-        box3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if(box3.isChecked()){
-                    box4.setChecked(false);
-                    box5.setChecked(false);
-                    box6.setChecked(false);
-                    name1.setVisibility(View.VISIBLE);
-                    name2.setVisibility(View.VISIBLE);
-                    name3.setVisibility(View.VISIBLE);
-                    name4.setVisibility(View.VISIBLE);
-                    name5.setVisibility(View.VISIBLE);
-                    name6.setVisibility(View.VISIBLE);
-                    name7.setVisibility(View.VISIBLE);
-                    name8.setVisibility(View.VISIBLE);
-                }
-            }
-        });
-        box4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(box4.isChecked()){
-                    box3.setChecked(false);
-                    box5.setChecked(false);
-                    box6.setChecked(false);
-                    name1.setVisibility(View.VISIBLE);
-                    name2.setVisibility(View.VISIBLE);
-                    name3.setVisibility(View.VISIBLE);
-                    name4.setVisibility(View.VISIBLE);
-                    name5.setVisibility(View.INVISIBLE);
-                    name6.setVisibility(View.INVISIBLE);
-                }
-            }
-        });
-
-        box5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(box5.isChecked()) {
-                    box4.setChecked(false);
-                    box3.setChecked(false);
-                    box6.setChecked(false);
-                    name1.setVisibility(View.VISIBLE);
-                    name2.setVisibility(View.VISIBLE);
-                    name3.setVisibility(View.VISIBLE);
-                    name4.setVisibility(View.VISIBLE);
-                    name5.setVisibility(View.VISIBLE);
-                    name6.setVisibility(View.INVISIBLE);
-                }
-            }
-        });
-
-        box6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(box6.isChecked()) {
-                    box4.setChecked(false);
-                    box5.setChecked(false);
-                    box3.setChecked(false);
-                    name1.setVisibility(View.VISIBLE);
-                    name2.setVisibility(View.VISIBLE);
-                    name3.setVisibility(View.VISIBLE);
-                    name4.setVisibility(View.VISIBLE);
-                    name5.setVisibility(View.VISIBLE);
-                    name6.setVisibility(View.VISIBLE);
-                }
-            }
-        });
+        /*final CheckBox box3 = findViewById(R.id.checkBox3);
+        final CheckBox box4 = findViewById(R.id.checkBox4);
+        final CheckBox box5 = findViewById(R.id.checkBox5);
+        final CheckBox box6 = findViewById(R.id.checkBox6);*/
 
 
         Button EFE = findViewById(R.id.start);
         EFE.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (BRAZactiv.this, BRAZactiv8.class);
-                intent.putExtra("NameOfTeam1",name1.getText().toString());
-                intent.putExtra("NameOfTeam2",name2.getText().toString());
-                intent.putExtra("NameOfTeam3",name3.getText().toString());
-                intent.putExtra("NameOfTeam4",name4.getText().toString());
-                intent.putExtra("NameOfTeam5",name5.getText().toString());
-                intent.putExtra("NameOfTeam6",name6.getText().toString());
-                intent.putExtra("NameOfTeam7",name7.getText().toString());
-                intent.putExtra("NameOfTeam8",name8.getText().toString());
-                startActivity(intent);
+                boolean checkNull=true;
+                for(int i =0;i<names.length;i++){
+                    if(names[i]==null)checkNull=false;
+                }
+                if(checkNull) {
+                    Intent intent = new Intent(BRAZactiv.this, BRAZactiv8.class);
+                    intent.putExtra("NameOfTeam1", customAdapter.getItem(0));
+                    intent.putExtra("NameOfTeam2", customAdapter.getItem(1));
+                    intent.putExtra("NameOfTeam3", customAdapter.getItem(2));
+                    intent.putExtra("NameOfTeam4", customAdapter.getItem(3));
+                    intent.putExtra("NameOfTeam5", customAdapter.getItem(4));
+                    intent.putExtra("NameOfTeam6", customAdapter.getItem(5));
+                    intent.putExtra("NameOfTeam7", customAdapter.getItem(6));
+                    intent.putExtra("NameOfTeam8", customAdapter.getItem(7));
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(),"Podaj nazwiska wszystkich graczy",Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
+    }
+
+    @Override
+    protected void onPause() {
+        View current = getCurrentFocus();
+        if (current != null) current.clearFocus();
+        super.onPause();
+    }
+
+
+    class CustomAdapter extends BaseAdapter {
+        private Context context;
+        private String[] rValues = {"R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15", "R16", "R17", "R18", "R19", "R20", "R21", "R22", "R23", "R24"};
+
+        CustomAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return getCount();
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return position;
+        }
+
+        @Override
+        public int getCount() {
+            return quantityOfTeam;
+        }
+
+        @Override
+        public String getItem(int position) {
+            return names[position] +"\n" + names[position + quantityOfTeam];
+        }
+
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            final ViewHolder holder;
+
+            if (convertView == null) {
+                holder = new ViewHolder();
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.brazylian_item_list, null, true);
+
+                names = new String[quantityOfTeam * 2];
+                holder.editText = convertView.findViewById(R.id.brazItemListEditTextName1);
+                holder.editText2 = convertView.findViewById(R.id.brazItemListEditTextName2);
+                holder.textView = convertView.findViewById(R.id.brazItemListtextViewR);
+
+
+
+                convertView.setTag(holder);
+            } else {
+                // the getTag returns the viewHolder object set as a tag to the view
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.textView.setText(rValues[position]);
+
+
+
+            holder.editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View view, boolean b) {
+                    if (!b) {
+                        names[position] = holder.editText.getText().toString();
+
+                    }
+                }
+            });
+
+            holder.editText2.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View view, boolean b) {
+                    if (!b) {
+                        names[position + quantityOfTeam] = holder.editText2.getText().toString();
+                    }
+                }
+            });
+
+            return convertView;
+        }
+
+        private class ViewHolder {
+
+            protected EditText editText;
+            protected EditText editText2;
+            protected TextView textView;
+
+        }
+
     }
 }
